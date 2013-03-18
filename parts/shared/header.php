2d9e0ea6dbdebeb53dc24fe5231929543c381195
John Yoder <johngilesyoder@gmail.com>
2013-03-17 18:16:10 -0600

    Initial commit

---
+++ parts/shared/header.php
@@ -0,0 +1,7 @@
+	<!-- Header
+	================================================== -->
+
+	<header>
+		<h1><a href="<?php echo home_url(); ?>"><?php bloginfo( 'name' ); ?></a></h1>
+		<?php bloginfo( 'description' ); ?>
+	</header>
